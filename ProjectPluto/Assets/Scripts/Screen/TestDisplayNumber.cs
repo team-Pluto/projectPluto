bc6a81dfa23635cf52fa09e05e4021fbc4cdc70e
Calvin So <ccs403@nyu.edu>
2019-05-15 23:59:58 -0400

    Basic screen interaction demo with pan camera

---
+++ ProjectPluto/Assets/Scripts/Screen/TestDisplayNumber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Just a simple test script to test some screen interactions
+/// </summary>
+public class TestDisplayNumber : MonoBehaviour
+{
+    Text textComponent;
+    
+    /// <summary>
+    /// Set the text to the given text.
+    /// </summary>
+    /// <param name="in_text"></param>
+    public void SetText(string in_text)
+    {
+        if(textComponent == null)
+        {
+            textComponent = GetComponent<Text>();
+        }
+
+        if(textComponent != null)
+        {
+            textComponent.text = in_text;
+        }
+    }
+}
